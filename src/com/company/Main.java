42c5a940eee61db8e351cbfc4991d82c357a317b
zhoy <artyomzhoy@gmail.com>
2021-06-07 19:20:32 +0300

    first commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,40 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        int[] sortedArray = new int[100]; // массив из 100 элементов int
+
+        int first = 0; //первый элемент массива
+        int last = sortedArray.length - 1; //последний элемент массива
+
+        for (int a = 0; a < sortedArray.length; a++) {
+            sortedArray[a] = a + 1;
+        }
+
+        binarySearch(sortedArray, 5, first, last);
+    }
+
+    public static void binarySearch(int[] sortedArray, int key, int low, int high) {
+
+        int index = 0;
+        int comparisonCount = 0; // для подсчета количества сравнений
+
+        while (low <= high) {
+            comparisonCount++;
+            int mid = (low + high) / 2; // центр массива
+            if (sortedArray[mid] < key) {
+                low = mid + 1;
+            } else if (sortedArray[mid] > key) { //
+                high = mid - 1;
+            } else if (sortedArray[mid] == key) {
+                index = mid;
+                System.out.println("Метод бинарного поиска нашел число после " + comparisonCount +
+                        " сравнений");
+                System.out.println("Индекс числа " + key + " в массиве – " + index );
+                break;
+            }
+        }
+    }
+}
+
